715b737df08ac9d635343fdae1416cb6347915e0
Devananda Jyothi <devanandajyothi2005@gmail.com>
2025-10-27 12:42:27 +0530

    new commit

--- link.c
+++ link.c
@@ -4,6 +4,7 @@
 
 #define DISK_SIZE 100
 #define MAX_NAME_LEN 50
+#define MAX_FILES 20
 
 int disk[DISK_SIZE];
 
@@ -21,81 +22,97 @@ int main() {
     // Initialize disk
     for (int i = 0; i < DISK_SIZE; i++) disk[i] = 0;
 
-    struct File file;
-    printf("Enter file name: ");
-    scanf("%s", file.name);
+    struct File files[MAX_FILES];
+    int n;
 
-    int num_blocks;
-    printf("Enter number of blocks: ");
-    scanf("%d", &num_blocks);
+    printf("Enter number of files: ");
+    scanf("%d", &n);
 
-    struct Block* head = NULL;
-    struct Block* current = NULL;
+    for (int f = 0; f < n; f++) {
+        printf("\nEnter file name: ");
+        scanf("%s", files[f].name);
 
-    printf("Enter %d block numbers:\n", num_blocks);
-    for (int i = 0; i < num_blocks; i++) {
-        int blockNumber;
-        scanf("%d", &blockNumber);
+        int num_blocks;
+        printf("Enter number of blocks for '%s': ", files[f].name);
+        scanf("%d", &num_blocks);
 
-        if (blockNumber < 0 || blockNumber >= DISK_SIZE) {
-            printf("Block %d is out of range!\n", blockNumber);
-            // Cleanup
+        struct Block* head = NULL;
+        struct Block* current = NULL;
+
+        printf("Enter %d block numbers:\n", num_blocks);
+        int success = 1;
+        for (int i = 0; i < num_blocks; i++) {
+            int blockNumber;
+            scanf("%d", &blockNumber);
+
+            if (blockNumber < 0 || blockNumber >= DISK_SIZE) {
+                printf("Block %d is out of range!\n", blockNumber);
+                success = 0;
+                break;
+            }
+            if (disk[blockNumber] == 1) {
+                printf("Block %d is already allocated!\n", blockNumber);
+                success = 0;
+                break;
+            }
+
+            struct Block* newBlock = (struct Block*)malloc(sizeof(struct Block));
+            newBlock->block_number = blockNumber;
+            newBlock->next = NULL;
+            disk[blockNumber] = 1;
+
+            if (head == NULL) {
+                head = newBlock;
+                current = newBlock;
+            } else {
+                current->next = newBlock;
+                current = current->next;
+            }
+        }
+
+        if (!success) {
+            // Cleanup any allocated blocks for this file
             while (head != NULL) {
                 struct Block* temp = head;
                 disk[head->block_number] = 0;
                 head = head->next;
                 free(temp);
             }
-            return 1;
-        }
-        if (disk[blockNumber] == 1) {
-            printf("Block %d is already allocated!\n", blockNumber);
-            // Cleanup
-            while (head != NULL) {
-                struct Block* temp = head;
-                disk[head->block_number] = 0;
-                head = head->next;
-                free(temp);
-            }
-            return 1;
+            printf("File '%s' allocation aborted. Please re-enter.\n", files[f].name);
+            f--; // retry this file
+            continue;
         }
 
-        struct Block* newBlock = (struct Block*)malloc(sizeof(struct Block));
-        newBlock->block_number = blockNumber;
-        newBlock->next = NULL;
-        disk[blockNumber] = 1;
-
-        if (head == NULL) {
-            head = newBlock;
-            current = newBlock;
-        } else {
-            current->next = newBlock;
-            current = current->next;
-        }
+        files[f].head = head;
+        printf("File '%s' allocated successfully.\n", files[f].name);
     }
 
-    file.head = head;
-
-    printf("\nFile '%s' allocated successfully.\nBlocks: ", file.name);
-    struct Block* temp = file.head;
-    while (temp != NULL) {
-        printf("%d -> ", temp->block_number);
-        temp = temp->next;
+    // Display allocation info for all files
+    printf("\n--- Allocation Table ---\n");
+    for (int f = 0; f < n; f++) {
+        printf("File: %s\nBlocks: ", files[f].name);
+        struct Block* temp = files[f].head;
+        while (temp != NULL) {
+            printf("%d -> ", temp->block_number);
+            temp = temp->next;
+        }
+        printf("NULL\n");
     }
-    printf("NULL\n");
 
-    printf("Used blocks on disk: ");
+    printf("\nUsed blocks on disk: ");
     for (int i = 0; i < DISK_SIZE; i++) {
         if (disk[i] == 1) printf("%d ", i);
     }
     printf("\n");
 
-    // Free memory
-    temp = file.head;
-    while (temp != NULL) {
-        struct Block* next = temp->next;
-        free(temp);
-        temp = next;
+    // Free memory for all files
+    for (int f = 0; f < n; f++) {
+        struct Block* temp = files[f].head;
+        while (temp != NULL) {
+            struct Block* next = temp->next;
+            free(temp);
+            temp = next;
+        }
     }
 
     return 0;
