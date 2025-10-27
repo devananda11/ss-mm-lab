cae28276fdd81157912066209b76db4988f1a706
Devananda Jyothi <devanandajyothi2005@gmail.com>
2025-10-27 12:38:45 +0530

    new commit

--- ind.c
+++ ind.c
@@ -3,6 +3,7 @@
 
 #define DISK_SIZE 100
 #define MAX_NAME_LEN 50
+#define MAX_FILES 20
 
 int disk[DISK_SIZE];
 
@@ -14,53 +15,72 @@ struct File {
 };
 
 int main() {
-    struct File file;
+    struct File files[MAX_FILES];
+    int n;
+
     // Initialize disk
     for (int i = 0; i < DISK_SIZE; i++) disk[i] = 0;
 
-    printf("Enter file name: ");
-    scanf("%s", file.name);
+    printf("Enter number of files: ");
+    scanf("%d", &n);
 
-    printf("Enter number of data blocks required: ");
-    scanf("%d", &file.blockCount);
+    for (int f = 0; f < n; f++) {
+        printf("\nEnter file name: ");
+        scanf("%s", files[f].name);
 
-    // Allocate index block
-    int indexBlock = -1;
-    for (int i = 0; i < DISK_SIZE; i++) {
-        if (disk[i] == 0) {
-            indexBlock = i;
-            disk[i] = 1;
-            break;
+        printf("Enter number of data blocks required: ");
+        scanf("%d", &files[f].blockCount);
+
+        // Allocate index block
+        int indexBlock = -1;
+        for (int i = 0; i < DISK_SIZE; i++) {
+            if (disk[i] == 0) {
+                indexBlock = i;
+                disk[i] = 1;
+                break;
+            }
         }
-    }
-    if (indexBlock == -1) {
-        printf("No free block for index block.\n");
-        return 1;
-    }
-    file.indexBlock = indexBlock;
-
-    printf("Enter %d block numbers for data blocks (0-%d):\n", file.blockCount, DISK_SIZE-1);
-    for (int i = 0; i < file.blockCount; i++) {
-        int blockNum;
-        scanf("%d", &blockNum);
-        if (blockNum < 0 || blockNum >= DISK_SIZE || disk[blockNum] == 1) {
-            printf("Block %d is not free or invalid. Aborting.\n", blockNum);
-            // Free already allocated blocks
-            for (int j = 0; j < i; j++) disk[file.dataBlocks[j]] = 0;
-            disk[indexBlock] = 0;
-            return 1;
+        if (indexBlock == -1) {
+            printf("No free block for index block.\n");
+            f--; // retry this file
+            continue;
         }
-        file.dataBlocks[i] = blockNum;
-        disk[blockNum] = 1;
+        files[f].indexBlock = indexBlock;
+
+        printf("Enter %d block numbers for data blocks (0-%d):\n", files[f].blockCount, DISK_SIZE-1);
+        int success = 1;
+        for (int i = 0; i < files[f].blockCount; i++) {
+            int blockNum;
+            scanf("%d", &blockNum);
+            if (blockNum < 0 || blockNum >= DISK_SIZE || disk[blockNum] == 1) {
+                printf("Block %d is not free or invalid. Aborting this file.\n", blockNum);
+                // Free already allocated blocks for this file
+                for (int j = 0; j < i; j++) disk[files[f].dataBlocks[j]] = 0;
+                disk[indexBlock] = 0;
+                success = 0;
+                break;
+            }
+            files[f].dataBlocks[i] = blockNum;
+            disk[blockNum] = 1;
+        }
+        if (!success) {
+            f--; // retry this file
+            continue;
+        }
+        printf("File '%s' allocated.\n", files[f].name);
     }
 
-    printf("\nFile '%s' allocated.\n", file.name);
-    printf("Index Block: %d\n", file.indexBlock);
-    printf("Data Blocks: ");
-    for (int i = 0; i < file.blockCount; i++) printf("%d ", file.dataBlocks[i]);
-    printf("\n");
+    // Display allocation info for all files
+    printf("\n--- Allocation Table ---\n");
+    for (int f = 0; f < n; f++) {
+        printf("File: %s\n", files[f].name);
+        printf("  Index Block: %d\n", files[f].indexBlock);
+        printf("  Data Blocks: ");
+        for (int i = 0; i < files[f].blockCount; i++) printf("%d ", files[f].dataBlocks[i]);
+        printf("\n");
+    }
 
-    printf("Used blocks: ");
+    printf("\nUsed blocks: ");
     for (int i = 0; i < DISK_SIZE; i++) if (disk[i]) printf("%d ", i);
     printf("\n");
 
